8487bcb1757fb97d66fb35239a1ce309f1b02cdb
maokimhuong19000 <maokimhuong19000.office@gmail.com>
2023-09-29 15:49:01 +0700

    290923

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1736,41 +1736,7 @@ class HomeController extends Controller
     /**===============Start Exoport Substance===================== */
     public function exsubstance()
     {
-        //$Material=Material::where('status',1)->get();
-        $Material = Aquota::join(
-            "comquotas",
-            "aquotas.id",
-            "=",
-            "comquotas.aquota_id",
-        )
-            //->join('customers','comquotas.customer_id','=','customers.id')
-            ->where("comquotas.customer_id", Auth::id())
-            ->where("year", date("Y"))
-            ->join("materials", "materials.id", "=", "aquotas.material_id")
-            ->get();
-        $countries = Country::where("status", 1)->get();
-        $entry = Port_Entry::where("status", 1)->get();
-        $Customer = Customer::with("Cominfo")->find(Auth::id());
-        $exportPort = Portexport::where("country_id", 1)->get(); // Portexport::all(); //Portexport::where('country_id',1);
-        $transport = Transport::all();
-        $cif = Incoterm::all();
-        $currency = Currency::all();
-        $uom = Uom::all();
-        //echo json_encode($exportPort);
-        return view(
-            "isubstance",
-            compact(
-                "Customer",
-                "Material",
-                "countries",
-                "entry",
-                "exportPort",
-                "transport",
-                "cif",
-                "currency",
-                "uom",
-            ),
-        );
+        echo'Hello Export Substance';
     }
 
 
@@ -1781,6 +1747,12 @@ class HomeController extends Controller
     {
     }
 
+
+    //test
+    public function isub_exsub()
+    {
+        return view("isub_exsub");
+    }
 
     /**===============End Exoport Substance===================== */
 
