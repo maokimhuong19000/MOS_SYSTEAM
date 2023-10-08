a74d286771c50c06cd6a298ae5a4f709cde4534a
maokimhuong19000 <maokimhuong19000.office@gmail.com>
2023-10-09 00:05:03 +0700

    09102023

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -586,8 +586,9 @@ class HomeController extends Controller
         $currency = Currency::all();
         $uom = Uom::all();
         $invoice_value = Iinvoice::all();
+        $invoicevalue=$invoice_value::all();
         //echo json_encode($exportPort);
-        //dd($invoice_value);
+
         return view(
             "isubstance",
             compact(
@@ -856,7 +857,7 @@ class HomeController extends Controller
         );
         $materialrequest->request_no = "ReqS" . $str;
         $materialrequest->save();
-
+      //  dd($materialrequest);
         foreach ($request->input("number") as $index => $value) {
             if ($value != null) {
                 // check not null request value
@@ -1737,42 +1738,7 @@ class HomeController extends Controller
     /**===============Start Exoport Substance===================== */
     public function exsubstance()
     {
-        //$Material=Material::where('status',1)->get();
-        $Material = Aquota::join(
-            "comquotas",
-            "aquotas.id",
-            "=",
-            "comquotas.aquota_id",
-        )
-            //->join('customers','comquotas.customer_id','=','customers.id')
-            ->where("comquotas.customer_id", Auth::id())
-            ->where("year", date("Y"))
-            ->join("materials", "materials.id", "=", "aquotas.material_id")
-            ->get();
-        $countries = Country::where("status", 1)->get();
-        $entry = Port_Entry::where("status", 1)->get();
-        $Customer = Customer::with("Cominfo")->find(Auth::id());
-        $exportPort = Portexport::where("country_id", 1)->get(); // Portexport::all(); //Portexport::where('country_id',1);
-        $transport = Transport::all();
-        $cif = Incoterm::all();
-        $currency = Currency::all();
-        $uom = Uom::all();
-        //echo json_encode($exportPort);
-        return view(
-            "exsubstance",
-            compact(
-                "Customer",
-                "Material",
-                "countries",
-                "entry",
-                "exportPort",
-                "transport",
-                "cif",
-                "currency",
-                "uom",
-            ),
-        );
-      
+       
     }
 
 
